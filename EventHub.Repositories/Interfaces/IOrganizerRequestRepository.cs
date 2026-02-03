5741f2a37925ecdcd37152f42cf33a1d5b1e146b
Toma Andreev <86717095+Tomkata@users.noreply.github.com>
2026-02-03 08:17:10 +0200

    Finalize repository for organizer request

--- EventHub.Repositories/Interfaces/IOrganizerRequestRepository.cs
+++ EventHub.Repositories/Interfaces/IOrganizerRequestRepository.cs
@@ -2,15 +2,14 @@
 namespace EventHub.Repositories.Interfaces
 {
     using EventHub.Core.Models;
-    using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 
     public interface IOrganizerRequestRepository
     {
         Task AddAsync(OrganizerRequest organizer);
 
-        Task<OrganizerRequest> GetByUserIdAsync(string id);
+        Task<OrganizerRequest?> GetByUserIdAsync(string id);
         Task SaveChangesAsync();
 
-        Task<IEnumerable<OrganizerRequest>> GetPengingRequestsAsync();
+        Task<IEnumerable<OrganizerRequest>> GetPendingRequestsAsync();
     }
 }
---
+++ EventHub.Repositories/Repositories/OrganizerRequestRepository.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace EventHub.Repositories.Repositories
+{
+    using EventHub.Core.Enums.Organizer;
+    using EventHub.Core.Models;
+    using EventHub.Infrastructure.Data;
+    using EventHub.Repositories.Interfaces;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+
+    public class OrganizerRequestRepository : IOrganizerRequestRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public OrganizerRequestRepository(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task AddAsync(OrganizerRequest organizer)
+            => await _dbContext.OrganizerRequests.AddAsync(organizer);
+
+        public async Task<OrganizerRequest?> GetByUserIdAsync(string id)=>
+            await _dbContext.OrganizerRequests
+                .FirstOrDefaultAsync(x => x.UserId == id);
+
+
+        public async Task<IEnumerable<OrganizerRequest>> GetPendingRequestsAsync()
+            => await _dbContext.OrganizerRequests
+            .AsNoTracking()
+            .Where(x => x.Status == Status.Pending)
+            .ToListAsync();
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
